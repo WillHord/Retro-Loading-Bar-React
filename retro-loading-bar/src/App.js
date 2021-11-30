4dee2b326c78e1c887f6f068af3f0d2cd344b65a
Will Hord <whord@ucsc.edu>
2021-11-29 20:42:29 -0800

    First working iteration:

---
+++ retro-loading-bar/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import LoadingBar from './LoadingBar'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="Background">
+      <LoadingBar/>
+    </div>
+  );
+}
+
+export default App;
